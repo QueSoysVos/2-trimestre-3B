2e180c1cc9a06ce8eb0678c01119f79f4be2d0a2
QueSoysVos <142410318+QueSoysVos@users.noreply.github.com>
2024-07-03 12:37:36 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".perguntas");
+const caixaAlternativas = document.querySelector(".caixa-palternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    "pergunta 1",
+    "pergunta 2"
+    ]
